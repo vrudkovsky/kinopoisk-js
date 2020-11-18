3b3049b75ef991e64d1d97afb95af180e2deefd6
Victor Rudkovsky <vpcomtek@gmail.com>
2020-11-18 07:13:23 +0200

    --youtube video

--- js/fetch.js
+++ js/fetch.js
@@ -74,88 +74,102 @@ function showFullInfo() {
     }
     
     fetch(url)
-    .then(function(value) {
-        if (value.status !== 200) {
-            return Promise.reject(new Error(value.status));
-        } 
-        return value.json();
-    })
-    .then(function(output) {
-        console.log(output);
-        movie.innerHTML = `
-        <h4 class="col-12 text-center text-info">${output.name || output.title}</h4>
-        <div class="col-4">
-        <img src='${urlPoster + output.poster_path}' alt='${output.name || output.title}'>
-        ${(output.homepage) ? `<p class='text-center'><a href="${output.homepage}" target="_blank">Official page</a></p>` : ''} 
-        ${(output.homepage) ? `<p class='text-center'><a href="https://imdb.com/title/${output.homepage}">IMDB link</a></p>` : ''} 
-        </div>
-        <div class="col-8">
-        <p>Rating: ${output.vote_average}</p>
-        <p>Status: ${output.status}</p>
-        <p>Premiere: ${output.first_air_date || output.release_date}</p>
-        ${(output.last_episode_to_air) ? `<p>${output.number_of_seasons} season; ${output.last_episode_to_air.episode_number} episodes came out;</p>` : '' }
-        <p>Description: ${output.overview}</p>
-        <br>
-        <div class="youtube"></div>
-        </div>
-        `;
-        
-        getVideo(this);
+        .then(function(value) {
+            if (value.status !== 200) {
+                return Promise.reject(new Error(value.status));
+            } 
+            return value.json();
+        })
+        .then(function(output) {
+            // console.log(output);
+            movie.innerHTML = `
+            <h4 class="col-12 text-center text-info">${output.name || output.title}</h4>
+            <div class="col-4">
+            <img src='${urlPoster + output.poster_path}' alt='${output.name || output.title}'>
+            ${(output.homepage) ? `<p class='text-center'><a href="${output.homepage}" target="_blank">Official page</a></p>` : ''} 
+            ${(output.homepage) ? `<p class='text-center'><a href="https://imdb.com/title/${output.homepage}">IMDB link</a></p>` : ''} 
+            </div>
+            <div class="col-8">
+            <p>Rating: ${output.vote_average}</p>
+            <p>Status: ${output.status}</p>
+            <p>Premiere: ${output.first_air_date || output.release_date}</p>
+            ${(output.last_episode_to_air) ? `<p>${output.number_of_seasons} season; ${output.last_episode_to_air.episode_number} episodes came out;</p>` : '' }
+            <p>Description: ${output.overview}</p>
+            <br>
+            <div class="youtube"></div>
+            </div>
+            `;
             
-        })	
-        .catch(function(reason) {
-            movie.innerHTML = 'Upps! Something goes wrong!!!';
-            console.log(reason);
-        });
+            getVideo();
+            console.log(this);
+                
+            })	
+            .catch(function(reason) {
+                movie.innerHTML = 'Upps! Something goes wrong!!!';
+                console.log(reason);
+            });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Page has been loaded');
     
     fetch('https://api.themoviedb.org/3/trending/all/day?api_key=5ca5769f4cf4fbeb1cab5ffdc28fd011')
-    .then(function(value) {
-        if (value.status !== 200) {
-            return Promise.reject(new Error(value.status));
-        } 
-        return value.json();
-    })
-    .then(function(output) {
-        let inner = '<h2 class="col-12 text-center text-info">Popular items of the week</h2>';
-        if (output.results.length === 0) {
-            inner = '<h2 class="col-12 text-center  text-info">There is no result on your search</h2>';
-        };
-        output.results.forEach(function (item) {
-            let name = item.name || item.title;
-            let img = item.poster_path;
-            const poster = img ? urlPoster + img : './img/noposter.jpg';
-            // let date = item.first_air_date || item.release_date;
-            let dataInfo = '';
-            if (item.media_type != 'person') {
-                dataInfo = `data-id="${item.id}" data-type="${item.media_type}"`;
-            }
-            inner += `
-            <div class="col-12 col-md-6 col-xl-3 item">
-                <img src="${poster}" class="img_poster" alt="${name}" ${dataInfo}></img> 
-                <h5>${name}</h5>
-            </div>
-            `;
+        .then(function(value) {
+            if (value.status !== 200) {
+                return Promise.reject(new Error(value.status));
+            } 
+            return value.json();
+        })
+        .then(function(output) {
+            let inner = '<h2 class="col-12 text-center text-info">Popular items of the week</h2>';
+            if (output.results.length === 0) {
+                inner = '<h2 class="col-12 text-center  text-info">There is no result on your search</h2>';
+            };
+            output.results.forEach(function (item) {
+                let name = item.name || item.title;
+                let img = item.poster_path;
+                const poster = img ? urlPoster + img : './img/noposter.jpg';
+                // let date = item.first_air_date || item.release_date;
+                let dataInfo = '';
+                if (item.media_type != 'person') {
+                    dataInfo = `data-id="${item.id}" data-type="${item.media_type}"`;
+                }
+                inner += `
+                <div class="col-12 col-md-6 col-xl-3 item">
+                    <img src="${poster}" class="img_poster" alt="${name}" ${dataInfo}></img> 
+                    <h5>${name}</h5>
+                </div>
+                `;
+            });
+            movie.innerHTML = inner;
+
+            
+            
+
+            addEventMedia();
+
+        })
+        .catch(function(reason) {
+            movie.innerHTML = 'Upps! Something goes wrong!!!';
+            console.log(reason);
         });
-        movie.innerHTML = inner;
-
-        
-        
-
-        addEventMedia();
-
-    })
-    .catch(function(reason) {
-        movie.innerHTML = 'Upps! Something goes wrong!!!';
-        console.log(reason);
-    });
 });
 
-function getVideo(obj) {
+function getVideo() {
     let youtube = movie.querySelector('.youtube');
-    // youtube.innerHTML = obj;
-    // console.log(obj);
+    fetch(`https://api.themoviedb.org/3/movie/524047/videos?api_key=5ca5769f4cf4fbeb1cab5ffdc28fd011&language=en-US`)
+        .then((value) => {
+            if (value.status !== 200) {
+                return Promise.reject(new Error(value.status));
+            } 
+            return value.json();
+        })
+        .then((output) => {
+            // console.log(output);
+            // console.log(output.results[0]);
+        })
+        .catch(function(reason) {
+            youtube.innerHTML = 'Upps! Something goes wrong!!!';
+            console.log(reason);
+        });
 }
